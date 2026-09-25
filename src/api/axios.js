2575c2a0e38a3f364c546926a90bc3f4f70e7c9c
Govind Thakur <gpanchdewari@gmail.com>
2026-09-25 22:42:37 +0530

    hello

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const api = axios.create({
-  baseURL: "https://hexsoftware-backend-2.onrender.com",
+  baseURL: "https://hexsoftware-backend-2.onrender.com/api",
 });
 
 //"http://localhost:5000/api"  https://hexsoftware-backend-2.onrender.com
