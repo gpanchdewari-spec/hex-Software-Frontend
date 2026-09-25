fa1cef780c00e70564094a6756d469eaec62c8cf
Govind Thakur <gpanchdewari@gmail.com>
2026-09-25 22:29:17 +0530

    hiii

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://hexsoftware-backend-2.onrender.com",
 });
+
+//"http://localhost:5000/api"  https://hexsoftware-backend-2.onrender.com
+
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) config.headers.Authorization = `Bearer ${token}`;
